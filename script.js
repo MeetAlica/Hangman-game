37e12c4eeef62e6cf0668dffe9acb349f527d3cd
MeetAlica <feher.ali.96@gmail.com>
2023-07-23 14:42:32 +0200

    javascript-random szo kivalasztasa

---
+++ script.js
@@ -0,0 +1,38 @@
+const startButton = document.getElementById("start-button");
+startButton.addEventListener("click", randomWord);
+
+// Véletlenszerű szó kiválasztása, a betűknek divek létrehozása
+function randomWord() {
+  fetch("http://localhost:3000/words/")
+    .then(response => response.json())
+    .then(word => {
+      word = word[Math.floor(Math.random() * word.length)];
+    removeLetterBox()
+    createGame(word);
+    });
+}
+
+// Divek létrehozása a betűknek
+const solutionContainer = document.getElementById("solution-container");
+
+function createGame(str) {
+  let letterBox = document.createElement("div");
+  letterBox.id = "letter-box";
+  let i = 1;
+  while (i <= str.length) {
+    const letterDiv = document.createElement("div");
+    letterDiv.id = `${i}`;
+    letterDiv.textContent = str.at(i-1);
+    letterBox.append(letterDiv);
+    i++;
+  }
+  solutionContainer.append(letterBox);
+}
+
+// Előző játék kitörlése
+function removeLetterBox() {
+  let letterBox = document.getElementById("letter-box");
+  if (letterBox !== null) {
+    letterBox.remove();
+  }
+}
