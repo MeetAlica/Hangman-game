80eee845fb81388eaa9129b3a5ca24c4627e93ea
MeetAlica <feher.ali.96@gmail.com>
2024-03-08 02:43:53 +0100

    Created Lives component

--- Hangman-game/src/App.jsx
+++ Hangman-game/src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+import Lives from "./components/Lives";
+
 const App = () => {
+  const [lives, setLives] = useState(0);
+  setLives(10);
+  
   return (
     <div>
-      
+      <Lives lives={lives} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
